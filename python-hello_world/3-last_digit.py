44908c67b54b03d5fe97cfb33617eaa3c480cea9
Noha Hamdy <nohatota99@gmail.com>
2023-07-18 13:14:26 +0300

    Created a new file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,6 +7,6 @@ if int(last_digit) > 5:
     result="and is greater than 5"
 elif int(last_digit) == 0:
     result="and is 0"
-else:
+elif (int(last_digit) < 6) and (int(last_digit) != 0):
     result="and is less than 6 and not 0"
 print("Last digit of" ,str_num, "is", last_digit,result)
