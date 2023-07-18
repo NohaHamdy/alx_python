3b049034a857ee8cd7abbf339f8eb118036365d0
Noha Hamdy <nohatota99@gmail.com>
2023-07-18 13:28:06 +0300

    made my code as expected

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,6 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 str_num=str(number)
 last_digit = str(abs(number) % 10)
+if number < 0:
+        last_digit = "-" + last_digit
 if int(last_digit) > 5:
     result="and is greater than 5"
 elif int(last_digit) == 0:
