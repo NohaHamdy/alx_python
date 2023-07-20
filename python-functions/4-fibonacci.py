225f57e439c107f41a20bdb1b436fd0ac778ea86
Noha Hamdy <nohatota99@gmail.com>
2023-07-20 12:53:15 +0300

    4. Fibonacci Sequence Function

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,9 @@
+def fibonacci_sequence(n):
+    FL=[]
+    for index in range (n):
+        if index < 2:
+            value = index
+        else:
+            value = FL[index-1] + FL[index-2]
+        FL.append(value)
+    return (FL)
