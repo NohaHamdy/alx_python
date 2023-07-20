ec40d89e4fc3d91c1adb4a4f81082ac4c94c8b25
Noha Hamdy <nohatota99@gmail.com>
2023-07-20 13:10:12 +0300

    5. Prime Number Function

---
+++ python-functions/5-prime.py
@@ -0,0 +1,9 @@
+def is_prime(number): 
+    if (number == 1):
+        return (True)
+    else:
+        for i in range (10):
+            if ((number % i) == 0)
+                return (False)
+return (True)
+        
