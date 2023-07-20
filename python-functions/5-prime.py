db460b9cb0578b6c4411ee617ddb5db9a0ffb6b8
Noha Hamdy <nohatota99@gmail.com>
2023-07-20 03:17:39 -0700

    Fixed my function

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,9 +1,10 @@
 def is_prime(number): 
     if (number == 1):
         return (True)
+    elif (number == 0):
+        return (False)
     else:
-        for i in range (10):
-            if ((number % i) == 0)
+        for i in range (2,10):
+            if ((number % i) == 0):
                 return (False)
-return (True)
-        
+    return (True)
