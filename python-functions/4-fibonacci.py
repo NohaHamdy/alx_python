f8921d3e51f0697f904c4c37f8dec985f1e6d9e6
Noha Hamdy <nohatota99@gmail.com>
2023-07-20 12:55:37 +0300

    I'll push now

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,8 +2,8 @@ def fibonacci_sequence(n):
     FL=[]
     for index in range (n):
         if index < 2:
-            value = index
+            value = index;
         else:
-            value = FL[index-1] + FL[index-2]
+            value = FL[index-1] + FL[index-2];
         FL.append(value)
     return (FL)
