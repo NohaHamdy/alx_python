d5e7444f47524167f8037579ee902d29e52364de
Noha Hamdy <nohatota99@gmail.com>
2023-07-23 12:47:17 +0300

    Fixing things according to PEP8 styling guide

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-    
+
 if __name__ == "__main__":
     main()
