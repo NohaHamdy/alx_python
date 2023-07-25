ac65a3b5558f82affab4e14d62fec79c64bc7df2
Noha Hamdy <nohatota99@gmail.com>
2023-07-25 09:49:20 +0300

    Solving some issues

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python3
+
 import sys
-i = len(sys.argv) -1
-if i == 1 :
-    situation = "argument"
-else:
-    situation = "arguments"
-if i == 0:
-    following = "."
-else:
-    following = ":"
-print(i,situation,following,end="\n")
-for j in range(1,i+1):
-    print("{}: {}".format(j,sys.argv[j]))
+
+def main():
+    i = len(sys.argv) -1
+    if i == 1 :
+        situation = "argument"
+    else:
+        situation = "arguments"
+    if i == 0:
+        following = "."
+    else:
+        following = ":"
+    print(i,situation,following,end="\n")
+    for j in range(1,i+1):
+        print("{}: {}".format(j,sys.argv[j]))
+        
+if __name__ == "__main__":
+    main()
