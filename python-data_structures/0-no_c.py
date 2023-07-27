74445fc6052a988be42225f6cc0c55efb48a0f56
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 12:36:03 +0300

    The output makes no sence

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -4,7 +4,7 @@ def no_c(my_string):
     for i in range (L):
         if my_string[i] == "c" or my_string[i] == "C":
             if i == 0:
-                new_str = my_string[i + 1:]
+                new_str = my_string[1:]
             else:
                 new_str = my_string[:i] + my_string[i + 1:]      
     return new_str
