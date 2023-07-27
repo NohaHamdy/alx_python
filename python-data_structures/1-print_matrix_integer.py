959420d7a7ffc0d6c55442baa7c86e55fe277562
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 13:29:43 +0300

    Hope it works this time

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,5 +2,7 @@ def print_matrix_integer(matrix=[[]]):
     L = len(matrix)
     for i in range (L):
         for j in range (len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), sep=" ",end = "")
+            print("{:d}".format(matrix[i][j]), end = "")
+            if j != len(matrix[i])-1: # check if it's not the last element of the row
+                print(" ", end="")
         print()
