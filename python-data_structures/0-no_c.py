a8e65351d629137c905588f5e77f853dfcd3be79
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 12:32:17 +0300

    Fixing syntax errors

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,8 @@
 def no_c(my_string):
     L = len(my_string)
-    for i in range L:
-        if my_string[i] == "c" or my_string(i) == "C":
+    new_str = my_string
+    for i in range (L):
+        if my_string[i] == "c" or my_string[i] == "C":
             new_str = my_string[:i] + my_string[i + 1:]
             
     return new_str
