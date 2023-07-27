aa386a62f7e9ef94928035f6aa515189822cd14b
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 12:48:28 +0300

    Trying a different implementation

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,10 +1,17 @@
+#def no_c(my_string):
+#    L = len(my_string)
+#    new_str = my_string
+#    for i in range (L):
+#            if i == 0:
+#                new_str = new_str[1:]
+#            else:
+#                new_str = new_str[:i] + new_str[i + 1:]      
+#    return new_str
+
 def no_c(my_string):
     L = len(my_string)
-    new_str = my_string
-    for i in range (L):
-        if new_str[i] == "c" or new_str[i] == "C":
-            if i == 0:
-                new_str = new_str[1:]
-            else:
-                new_str = new_str[:i] + new_str[i + 1:]      
+    new_str = ""
+    for i in range(L):
+        if my_string[i] != "c" and my_string[i] != "C":
+            new_str += my_string[i]
     return new_str
