b5c8c1b9a80bf6368277cd031e705382ee899b98
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 12:43:19 +0300

    switched everything to the new str

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -2,9 +2,9 @@ def no_c(my_string):
     L = len(my_string)
     new_str = my_string
     for i in range (L):
-        if my_string[i] == "c" or my_string[i] == "C":
+        if new_str[i] == "c" or new_str[i] == "C":
             if i == 0:
-                new_str = my_string[1:]
+                new_str = new_str[1:]
             else:
                 new_str = new_str[:i] + new_str[i + 1:]      
     return new_str
