c6b55d588dddc9202139da655709002b4027c484
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 13:16:45 +0300

    Fixed some errors

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,4 +2,4 @@ def print_matrix_integer(matrix=[[]]):
     L = len(matrix)
     for i in range (L):
         for j in range (len(matrix[i])):
-            print("{}".format[i][j])
+            print("{:d}".format(matrix[i][j]), end = " ")
