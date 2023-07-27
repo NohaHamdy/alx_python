c73f09e18a2553bfc1ed6203965fb18b1c5bb84b
Noha Hamdy <nohatota99@gmail.com>
2023-07-27 13:21:31 +0300

    Let's try

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -3,4 +3,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in range (L):
         for j in range (len(matrix[i])):
             print("{:d}".format(matrix[i][j]), end = " ")
-        print("\n")
+        print()
