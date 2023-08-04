a04816b4630e26511040f64ee743c8cfe2b47f32
Noha Hamdy <nohatota99@gmail.com>
2023-08-04 13:34:28 +0300

    Check the size this time

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -9,9 +9,9 @@ class Square:
     Using init function inside the class
     """
     def __init__(self, size=0):
-        if not isinstance(self,int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif (size < 0) :
+        elif size < 0 :
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
