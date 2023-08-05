3f3818e03fa95224c241aeaa6f3a2c8a043aa2f7
Noha Hamdy <nohatota99@gmail.com>
2023-08-05 10:37:03 +0300

    What about now ?

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,6 +2,8 @@
 """
     Why size is private attribute?
     The size of a square is crucial for a square, many things depend of it (area computation, etc.), so you, as class builder, must control the type and value of this attribute. One way to have the control is to keep it privately. You will see in next tasks how to get, update and validate the size value.
+    Why a getter and setter?
+    Reminder: size is a private attribute. We did that to make sure we control the type and value. Getter and setter methods are not 100% Python, but more OOP. With them, you will be able to validate the assignment of a private attribute and also define how getting the attribute value will be available from outside - by copy? by assignment? etc. Also, adding type/value validation in the setter will centralize the logic, since you will do it in only one place.
 """
 
 class Square:
