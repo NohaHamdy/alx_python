88616aed9a9512a68de4486d33cce88ffefaa14d
Noha Hamdy <nohatota99@gmail.com>
2023-08-05 10:35:17 +0300

    Module now is documented

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,7 +10,10 @@ class Square:
     """
     def __init__(self, size=0):
         self.size = size
-        
+    """
+    Why a getter and setter?
+    Reminder: size is a private attribute. We did that to make sure we control the type and value. Getter and setter methods are not 100% Python, but more OOP. With them, you will be able to validate the assignment of a private attribute and also define how getting the attribute value will be available from outside - by copy? by assignment? etc. Also, adding type/value validation in the setter will centralize the logic, since you will do it in only one place.
+    """    
     @property
     def size(self):
         retuen self.__size
