05eb338fb31444e1cb910362267ac0fdac83c442
Noha Hamdy <nohatota99@gmail.com>
2023-08-06 13:43:38 +0300

    4. Printing a square

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Using the @property decorators 
+"""
+class Square:
+    """
+Getting the area size
+"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    """
+    Why a getter and setter?
+    Reminder: size is a private attribute. We did that to make sure we control the type and value. Getter and setter methods are not 100% Python, but more OOP. With them, you will be able to validate the assignment of a private attribute and also define how getting the attribute value will be available from outside - by copy? by assignment? etc. Also, adding type/value validation in the setter will centralize the logic, since you will do it in only one place.
+    """
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
