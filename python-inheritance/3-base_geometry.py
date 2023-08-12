ce34c9c04cf8a16d9210677d1fd52562713847fb
Noha Hamdy <nohatota99@gmail.com>
2023-08-12 11:25:15 +0300

    I don't know why

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,4 +4,28 @@
 """
 class BaseGeometry:
     """It is an empty class"""
-    pass
+    def __dir__(self):
+        """
+        Override the dir() method to exclude __init_subclass__ from the list of attributes for the instance.
+        """
+        attributes = super().__dir__()
+        # Exclude __init_subclass__ from the list of attributes for the instance
+        attributes = [attr for attr in attributes if attr != "__init_subclass__"]
+        return attributes
+
+
+def class_dir(cls):
+    """
+    Override the dir() method to exclude __init_subclass__ from the list of attributes for the class.
+    """
+    attributes = super(cls).__dir__()
+    # Exclude __init_subclass__ from the list of attributes for the class
+    attributes = [attr for attr in attributes if attr != "__init_subclass__"]
+    return attributes
+
+
+if __name__ == "__main__":
+    bg = BaseGeometry()
+    print(bg)
+    print(dir(bg))
+    print(class_dir(BaseGeometry()))
