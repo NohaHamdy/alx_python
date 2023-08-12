b8e927548aa1236d4d6a92ad48dfe9809a0918a2
Noha Hamdy <nohatota99@gmail.com>
2023-08-12 10:54:57 +0300

    I don't think so also

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,3 +4,14 @@
 """
 class BaseGeometry:
     """It is an empty class"""
+    def __init__(self):
+        """Initializes the BaseGeometry class."""
+        pass
+
+    def __repr__(self):
+        """Returns a string representation of the BaseGeometry class instance."""
+        return f"BaseGeometry()"
+
+    def __str__(self):
+        """Returns a human-readable string representation of the BaseGeometry class instance."""
+        return "This is an instance of the BaseGeometry class."
