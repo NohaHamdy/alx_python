1c1729e55ed5995c8e6335608085c8c13255a8aa
Noha Hamdy <nohatota99@gmail.com>
2023-08-12 10:39:41 +0300

    2. Only sub class of

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Checks object class"""
+
+def inherits_from(obj, a_class):
+    """
+    This function checks if a class is instance or what
+    
+    Parameters:
+    obj (object): The object to be checked.
+    a_class (class): The class to check against.
+
+    Returns:
+    bool: True if `obj` is an instance of `a_class`, False otherwise.
+    """
+    result = isinstance(obj, a_class) & type(obj) != a_class
+    return result
