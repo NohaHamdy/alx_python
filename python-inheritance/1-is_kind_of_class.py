e7e055e6cb8a95ec008bc5184fb6b0e180407f50
Noha Hamdy <nohatota99@gmail.com>
2023-08-12 10:34:13 +0300

    1. Same class or inherit from

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Checks object class"""
+
+def is_same_class(obj, a_class):
+    """
+    This function checks if a class is instance or what
+    
+    Parameters:
+    obj (object): The object to be checked.
+    a_class (class): The class to check against.
+
+    Returns:
+    bool: True if `obj` is an instance of `a_class`, False otherwise.
+    """
+    result = isinstance(obj, a_class)
+    return result
