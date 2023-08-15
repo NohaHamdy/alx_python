d666b64b5f59c311ad4da98c8ad07e9621760afe
Noha Hamdy <nohatota99@gmail.com>
2023-08-16 00:41:48 +0300

    Test

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Definition for an empty class called BaseGeometry."""
-class BaseGeometry(metaclass=CustomMeta):
+"""Defines an empty class BaseGeometry."""
+class CustomMeta(type):
+    """The custom meta class"""
+    def __dir__(cls):
+        """override dir() method for meta class"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+class BaseGeometry(metaclass = CustomMeta):
     """BaseGeometry class with the custom metaclass"""
     pass
 
     def __dir__(cls):
-        """Overrides dir() method for the class"""
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
-class CustomMeta(type):
-    """A custom metaclass"""
-    def __dir__(cls):
-        """Override dir() method for the metaclass"""
+        """Overrides dir() method for the metaclass"""
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
