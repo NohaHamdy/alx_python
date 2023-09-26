fff5dd52825e639ae6a07c020f899af028c570a3
Noha Hamdy <nohatota99@gmail.com>
2023-09-26 10:02:14 +0300

    4. Is it a number?

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" Five routes  """
+from flask import Flask, abort
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def task_0():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def task_1():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def task_2(text):
+    word = text.split('_')
+    return f"C {' '.join(word)}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def task_3(text='is cool'):
+    word = text.split('_')
+
+    return f"Python {' '.join(word)}"
+
+# show if n is integer
+@app.route('/number/<int:n>', strict_slashes=False)
+def task_4(n):
+    return f'{n} is a number'
+
+# show if n is float
+@app.route('/number/<float:n>', strict_slashes=False)
+def task_4_1(n):
+    abort(404)
+
+# show if n is string
+@app.route('/number/<string:n>', strict_slashes=False)
+def task_4_2(n):
+    abort(404)
+
+if __name__ == "__main__":
+    app.run(debug=True)
