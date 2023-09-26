ddaf0b5c9342bd1775f12fa01857462e07184891
Noha Hamdy <nohatota99@gmail.com>
2023-09-26 10:35:59 +0300

    1. Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""List all states with a name starting with N from the database"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id ASC;""")
+    rows = cur.fetchall()
+    for r in rows:
+        print(r)
