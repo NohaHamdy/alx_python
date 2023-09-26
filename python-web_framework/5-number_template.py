bcbdae9a17382a3872265776184fd65257d91e00
Noha Hamdy <nohatota99@gmail.com>
2023-09-26 10:06:55 +0300

    5. Number template

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Five routes included"""
+from flask import Flask, abort, render_template
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def task_0():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def task_1():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def task_2(text):
+    word = text.split('_')
+    return f"C {' '.join(word)}"
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def task_3(text='is cool'):
+    word = text.split('_')
+
+    return f"Python {' '.join(word)}"
+
+# show if n is integer
+@app.route('/number/<int:n>', strict_slashes=False)
+def task_4(n):
+    return f'{n} is a number'
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def task_5(n):
+    return render_template('5-number.html', num=n)
+
+if __name__ == "__main__":
+    app.run(debug=True)
