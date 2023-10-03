791f916bb5761397494dccc9143b7d8a2b35d550
Noha Hamdy <nohatota99@gmail.com>
2023-10-03 15:50:06 +0300

    PEP8 validation

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,12 +11,11 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name =%s\
                   ORDER BY states.id ASC", (sys.argv[4],))
-    
+
     rows = cur.fetchall()
-    
+
     for r in rows:
         print(r)
